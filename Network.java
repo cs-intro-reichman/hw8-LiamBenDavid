fcf966ccb7dad16b5843d74adf98db28f3dc60b2
LiamBenDavid <liamkhto12@gmail.com>
2026-01-04 17:21:42 +0200

    NetworkObject7thTry

--- Network.java
+++ Network.java
@@ -139,7 +139,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String result = "Network:\n"
+        String result = "Network:\n";
         for (int i = 0; i< userCount; i++){
             result += users[i].toString() + "\n";
         }
